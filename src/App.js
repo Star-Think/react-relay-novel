2682fcd3a419f3a5f6e2927594b9026d797e98c2
xkstndua15 <xkdtndua15@gmail.com>
2022-08-09 20:03:25 +0900

    App router 수정

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import "./asset/css/full.css";
-import "./asset/css/tailwind.min.css";
-import "./asset/css/font-awesome.css";
-import "./asset/css/App.css";
-import "aos/dist/aos.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RelayMain from "./pages/relay/RelayMain";
-import RelayRoute from "./routes/RelayRoute";
-import Home from "./pages/home/Home";
+import './asset/css/full.css';
+import './asset/css/tailwind.min.css';
+import './asset/css/font-awesome.css';
+import './asset/css/App.css';
+import 'aos/dist/aos.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RelayMain from './pages/relay/RelayMain';
+import RelayRoute from './routes/RelayRoute';
+import Home from './pages/home/Home';
+import { useEffect } from 'react';
+import EveryRoute from './routes/EveryRoute';
 
 function App() {
-  // useEffect(() => {
-  //   let list = [];
-  //   for (let i = 0; i < 100; i++) {
-  //     list.push({
-  //       idx: i,
-  //       title: `제목${i}`,
-  //       content: `내용${i}`,
-  //       date: 1658195117794,
-  //       user_name: "별생각",
-  //       views: i,
-  //       type: 0,
-  //       state: 0,
-  //     });
-  //   }
+	// useEffect(() => {
+	// 	let list = [];
+	// 	for (let i = 0; i < 100; i++) {
+	// 		list.push({
+	// 			idx: i,
+	// 			title: `제목${i}`,
+	// 			content: `내용${i}`,
+	// 			date: 1658195117794,
+	// 			user_name: '별생각',
+	// 			views: i,
+	// 			type: 0,
+	// 			state: 0,
+	// 		});
+	// 	}
 
-  //   console.log(list);
-  // }, []);
+	// 	console.log(list);
+	// }, []);
 
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/relay/*" element={<RelayRoute />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route path='/relay' element={<RelayRoute />} />
+					<Route path='/everydiary/*' element={<EveryRoute />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
