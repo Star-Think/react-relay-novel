0413cf2ce5cc2ff85d461aef1712be70b46e01a5
xkstndua15 <xkdtndua15@gmail.com>
2022-08-10 13:12:12 +0900

    메모 디자인 변경

--- src/components/common/MemoList.js
+++ src/components/common/MemoList.js
@@ -1,17 +1,48 @@
-import React from "react";
-import MemoItem from "./MemoItem";
-import MemoPage from "./MemoPage";
+import React from 'react';
+import MemoItem from './MemoItem';
+import MemoPage from './MemoPage';
+import React from 'react';
+import MemoItem from './MemoItem';
+import MemoPage from './MemoPage';
+import { useEffect, useState } from 'react';
+import { timeChange } from '../../utils/CommonFun';
 
 const MemoList = ({ memoDataList }) => {
-  return (
-    <>
-      <div className="container mx-auto flex flex-wrap justify-start">
-        {memoDataList.map((mData) => {
-          return <MemoItem key={mData.idx} memo={mData} />;
-        })}
-      </div>
-    </>
-  );
+	const [DateList, setDateList] = useState([]);
+	let dateList = [];
+
+	useEffect(() => {
+		memoDataList.forEach((memoData) => {
+			if (dateList[dateList.length - 1] != memoData.date) {
+				dateList.push(memoData.date);
+			}
+		});
+
+		setDateList(dateList);
+	}, []);
+
+	return (
+		<>
+			{DateList.length !== 0 ? (
+				<div>
+					{DateList.map((date) => {
+						return (
+							<div key={date}>
+								<p className='text-center'>{timeChange(date)}</p>
+								<div className='container mx-auto flex flex-wrap justify-start'>
+									{memoDataList.map((mData) => {
+										if (mData.date === date) {
+											return <MemoItem key={mData.idx} memo={mData} />;
+										}
+									})}
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			) : null}
+		</>
+	);
 };
 
 export default MemoList;
--- src/components/common/MemoPage.js
+++ src/components/common/MemoPage.js
@@ -1,42 +1,99 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const MemoPage = ({ memoDataCount }) => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const page = location.state !== null ? location.state.page : 1;
-  const [totalPage, setTotalPage] = useState(1);
+	const navigate = useNavigate();
+	const location = useLocation();
+	const page = location.state !== null ? location.state.page : 1;
+	const [totalPage, setTotalPage] = useState(1);
+	const [PageNum, setPageNum] = useState([]);
 
-  const pageCalculation = () => {
-    setTotalPage(Math.ceil(memoDataCount / 24.0));
-  };
+	const pageCalculation = () => {
+		setTotalPage(Math.ceil(memoDataCount / 24.0));
+	};
 
-  useEffect(() => {
-    pageCalculation();
-  }, [memoDataCount]);
+	const setPage = () => {
+		const cur = (page - 1) % 5;
+		const back = 5 - cur;
+		const front = 5 - back;
+		let numList = [];
+		for (let i = front; i > 0; i--) {
+			numList.push((page / 5) * 5 - i);
+		}
+		for (let i = 0; i < back; i++) {
+			if ((page / 5) * 5 + i > Math.ceil(memoDataCount / 24.0)) break;
+			numList.push((page / 5) * 5 + i);
+		}
 
-  return (
-    <div className="btn-group flex justify-center mt-20">
-      {page !== 1 ? (
-        <div
-          onClick={() => navigate("/relay", { state: { page: page - 1 } })}
-          className="btn"
-        >
-          &lt;&lt;
-        </div>
-      ) : null}
+		setPageNum(numList);
+	};
 
-      <div className="btn btn-primary">{page}</div>
-      {page !== totalPage ? (
-        <div
-          onClick={() => navigate("/relay", { state: { page: page + 1 } })}
-          className="btn"
-        >
-          &gt;&gt;
-        </div>
-      ) : null}
-    </div>
-  );
+	useEffect(() => {
+		pageCalculation();
+		setPage();
+	}, []);
+
+	return (
+		<div className='btn-group flex justify-center mt-20'>
+			{page !== 1 ? (
+				<div
+					onClick={() => navigate('/everydiary', { state: { page: 1 } })}
+					className='btn'>
+					&lt;&lt;
+				</div>
+			) : null}
+			{page !== 1 ? (
+				<div
+					onClick={() => navigate('/everydiary', { state: { page: page - 1 } })}
+					className='btn'>
+					&lt;
+				</div>
+			) : null}
+
+			{PageNum.map((pg) => {
+				return (
+					<div key={pg}>
+						{page === pg ? (
+							<div
+								className='btn btn-primary bg-black text-white'
+								onClick={() =>
+									navigate('/everydiary', { state: { page: pg } })
+								}>
+								{pg}
+							</div>
+						) : (
+							<div
+								className='btn btn-primary'
+								onClick={() =>
+									navigate('/everydiary', { state: { page: pg } })
+								}>
+								{pg}
+							</div>
+						)}
+					</div>
+				);
+			})}
+
+			{page !== totalPage ? (
+				<div
+					onClick={() => navigate('/everydiary', { state: { page: page + 1 } })}
+					className='btn'>
+					&gt;
+				</div>
+			) : null}
+			{page !== totalPage ? (
+				<div
+					onClick={() =>
+						navigate('/everydiary', { state: { page: totalPage } })
+					}
+					className='btn'>
+					&gt;&gt;
+				</div>
+			) : null}
+		</div>
+	);
 };
 
 export default MemoPage;
