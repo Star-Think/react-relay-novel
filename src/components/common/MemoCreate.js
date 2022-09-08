ea6c6ebca1c88749585813be664f410cac6b34f9
soeun <45187472+soeunb@users.noreply.github.com>
2022-09-09 00:50:33 +0900

    일기쓰기 diaryAdd api

--- src/components/common/MemoCreate.js
+++ src/components/common/MemoCreate.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
-import BasicTemplate from "../../components/templates/BasicTemplate";
+import { useNavigate } from "react-router-dom";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+import axios from "axios";
 
 const MemoCreate = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("access_token");
   const [state, setState] = useState({
-    isPublic: 0,
+    isPublic: "0",
     title: "",
     content: "",
   });
@@ -27,110 +32,93 @@ const MemoCreate = () => {
   };
 
   const handleSubmit = () => {
-    console.log("click");
-    //onCreate();
-    /* setState({
+    setData();
+    setState({
       isPublic: 0,
       title: "",
       content: "",
-    }); */
-    console.log("state", state);
+    });
   };
 
+  async function setData() {
+    try {
+      const response = await axios.post(
+        "/star/api/diaryAdd",
+        {
+          title: state.title,
+          content: state.content,
+          type: isPublic,
+        },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      navigate("/my");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <BasicTemplate
-      Content={() => {
-        return (
-          <>
-            <div className="flex justify-center mt-10" style={{ marginTop: "150px" }}>
-              <div className="lg:w-2/5 md:w-3/5 sm:w-4/5 w-full mx-5">
-                <input
-                  type="hidden"
-                  name="csrfmiddlewaretoken"
-                  value="clKaGBzdOFuWeZrJGFDprKHWxH2IQAZedoW0uLybMFILimFKAArw2IAO2MVnJFez"
-                />
-                <p>
-                  <input
-                    type="text"
-                    name="author"
-                    required
-                    id="id_author"
-                    value="2616"
-                    readOnly
-                    hidden
-                  />
-                </p>
-                <p>
-                  <input
-                    type="text"
-                    name="topic"
-                    required
-                    id="id_topic"
-                    value="1"
-                    readOnly
-                    hidden
-                  />
-                </p>
-                <div className="flex justify-end">
-                  <div
-                    id="id_postPublicTooltip"
-                    className="tooltip tooltip-open tooltip-top tooltip-info"
-                    data-tip="누구나 일기를 읽을 수 있어요.">
-                    <p>
-                      <select
-                        name="isPublic"
-                        className="select select-bordered select-primary w-full max-w-xs"
-                        id="id_public"
-                        onChange={handleChangeState}
-                        value={state.isPublic}>
-                        {options.map((item, index) => (
-                          <option key={item.key} value={item.key}>
-                            {item.value}
-                          </option>
-                        ))}
-                      </select>
-                    </p>
-                  </div>
-                </div>
-                <p>
-                  <input
-                    type="text"
-                    name="title"
-                    className="input input-primary input-bordered my-2"
-                    placeholder="제목"
-                    style={{ width: "100%" }}
-                    maxLength="40"
-                    autoComplete="off"
-                    id="id_title"
-                    required
-                    onChange={handleChangeState}
-                    value={title}
-                  />
-                </p>
-                <p>
-                  <textarea
-                    name="content"
-                    className="input input-primary input-bordered my-2"
-                    placeholder="내용"
-                    style={{ width: "100%", height: "400px" }}
-                    required
-                    id="id_content"
-                    onChange={handleChangeState}
-                    value={content}
-                  />
-                </p>
-                <div className="flex justify-end">
-                  <div onClick={handleSubmit} className="btn btn-primary">
-                    저장
-                  </div>
-                </div>
-              </div>
+    <>
+      <Header />
+      <div className="flex justify-center mt-10" style={{ marginTop: "150px" }}>
+        <div className="lg:w-2/5 md:w-3/5 sm:w-4/5 w-full mx-5">
+          <div className="flex justify-end">
+            <div
+              id="id_postPublicTooltip"
+              className="tooltip tooltip-open tooltip-top tooltip-info"
+              data-tip="누구나 일기를 읽을 수 있어요.">
+              <p>
+                <select
+                  name="isPublic"
+                  className="select select-bordered select-primary w-full max-w-xs"
+                  id="id_public"
+                  onChange={handleChangeState}
+                  value={state.isPublic}>
+                  {options.map((item, index) => (
+                    <option key={item.key} value={item.key}>
+                      {item.value}
+                    </option>
+                  ))}
+                </select>
+              </p>
             </div>
-            ;
-          </>
-        );
-      }}
-    />
+          </div>
+          <p>
+            <input
+              type="text"
+              name="title"
+              className="input input-primary input-bordered my-2"
+              placeholder="제목"
+              style={{ width: "100%" }}
+              maxLength="40"
+              autoComplete="off"
+              id="id_title"
+              required
+              onChange={handleChangeState}
+              value={title}
+            />
+          </p>
+          <p>
+            <textarea
+              name="content"
+              className="input input-primary input-bordered my-2"
+              placeholder="내용"
+              style={{ width: "100%", height: "400px" }}
+              required
+              id="id_content"
+              onChange={handleChangeState}
+              value={content}
+            />
+          </p>
+          <div className="flex justify-end">
+            <div onClick={handleSubmit} className="btn btn-primary">
+              저장
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
