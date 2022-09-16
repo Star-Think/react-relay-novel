06426123eb685af62f95144803c9697bda2bff70
7ingout <hadvjin@naver.com>
2022-09-16 16:36:29 +0900

    mypage

--- src/pages/mypage/Dropout.js
+++ src/pages/mypage/Dropout.js
@@ -15,24 +15,24 @@ const Dropout = () => {
     return (
         <div>
             <Header />
-            <div class="flex justify-center mt-20 mb-20 mx-10" style={marginTop}>
+            <div className="flex justify-center mt-20 mb-20 mx-10" style={marginTop}>
                 <div>
-                    <h3 class="text-4xl text-error text-center mb-10">꼭 읽어주세요.</h3>
-                    <ul class="text-lg">
+                    <h3 className="text-4xl text-error text-center mb-10">꼭 읽어주세요.</h3>
+                    <ul className="text-lg">
                         <li>- 탈퇴된 계정은 절대 복구할 수 없습니다.</li>
                         <li>- 작성한 일기와 댓글이 모두 삭제됩니다.</li>
                         <li>- 개설한 학급이 있는 경우 학급도 삭제됩니다.</li>
                     </ul>
                 </div>
             </div>
-                <div class="flex justify-center text-3xl mb-10">
+                <div className="flex justify-center text-3xl mb-10">
                         정말 탈퇴하시겠습니까?
                     </div>
-                <div class="flex justify-center">
+                <div className="flex justify-center">
                 <form method="post">
                     <input type="hidden" name="csrfmiddlewaretoken" value="85z7QUcSFb05WGBafjTi26msXTcaxznGBpvuUSyTWXrgH5VOXmxOxRJSWVmDr20A"/>
-                    <div class="btn mr-10" onClick={back}>취소</div>
-                    <button type="submit" class="btn btn-error ml-10">탈퇴</button>
+                    <div className="btn mr-10" onClick={back}>취소</div>
+                    <button type="submit" className="btn btn-error ml-10">탈퇴</button>
                 </form>
             </div>
             <Footer/>
