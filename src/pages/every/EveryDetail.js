d29517cf46f15c266b3754dede68dbb3bc7ad891
soeun <45187472+soeunb@users.noreply.github.com>
2022-09-22 16:01:20 +0900

    댓글 클릭 > 없는 메모일 경우 수정

--- src/pages/every/EveryDetail.js
+++ src/pages/every/EveryDetail.js
@@ -8,12 +8,16 @@ import BasicTemplate from "../../components/templates/BasicTemplate";
 const EveryDetail = () => {
   const navigate = useNavigate();
   const { idx } = useParams();
-  const location = useLocation();
-  const data = Array.isArray(location.state.data) ? location.state.data : [location.state.data];
-
+  const { state } = useLocation();
   const [memo, setMemo] = useState({});
 
   useEffect(() => {
+    if (!state || !state.data) {
+      alert("없는 메모입니다.");
+      navigate(-2);
+      return;
+    }
+    const data = Array.isArray(state.data) ? state.data : [state.data];
     const memoObj = data.find((it) => it.seq === parseInt(idx));
     if (memoObj) {
       setMemo(memoObj);
--- src/pages/my/MyDetail.js
+++ src/pages/my/MyDetail.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams, useLocation } from "react-router-dom";
 import MemoDetail from "../../components/common/MemoDetail";
-import MemoReprotModal from "../../components/common/MemoReprotModal";
 import CommentCreate from "../../components/common/CommentCreate";
 import BasicTemplate from "../../components/templates/BasicTemplate";
 
 const MyDetail = () => {
   const navigate = useNavigate();
-  const location = useLocation();
-  const data = Array.isArray(location.state.data) ? location.state.data : [location.state.data];
   const { idx } = useParams();
+  const { state } = useLocation();
   const [memo, setMemo] = useState({});
 
   useEffect(() => {
+    if (!state || !state.data) {
+      alert("없는 메모입니다.");
+      navigate(-2);
+      return;
+    }
+    const data = Array.isArray(state.data) ? state.data : [state.data];
     const memoObj = data.find((it) => it.seq === parseInt(idx));
     if (memoObj) {
       setMemo(memoObj);
