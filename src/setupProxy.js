170ae21fe2234009a0bef3b39ef656f74cac109c
kimjonghyo <qaws2921@hanmail.net>
2022-09-22 09:21:37 +0900

    도메인 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/star", {
-      target: "http://star-think.ddns.net:1722",
+      // target: "http://star-think.ddns.net:1722",
+      target: "http://122.39.136.92:1722",
       pathRewrite: {
         "^/star": "",
       },
