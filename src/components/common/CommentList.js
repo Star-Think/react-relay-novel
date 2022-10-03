28d4f9996c05ecc2e5d21ef8a88c5faf9f99ad35
soeun <45187472+soeunb@users.noreply.github.com>
2022-10-03 21:06:04 +0900

    상대방 마이페이지 댓글 버튼 오류 수정

--- src/components/common/CommentList.js
+++ src/components/common/CommentList.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import store from "../../store";
 
 const CommentList = ({ commentList, memoDataList, path }) => {
   const location = useLocation();
   const navigate = useNavigate();
   const pathName = location.pathname;
+  const viewId = location.state.viewId;
+  const userId = store.getState().user.userInfo.user_id;
   const receivedComment = pathName.includes("received");
   const memoData = memoDataList.filter((elem) => {
     return elem.seq === commentList[0].story_seq;
   });
+
+  const commentTitleText = () => {
+    if (receivedComment) {
+      return `[내가 받은 댓글]`;
+    } else {
+      if (viewId && viewId !== userId) {
+        return `[${viewId}가 쓴 댓글]`;
+      } else {
+        return `[내가 쓴 댓글]`;
+      }
+    }
+  };
 
   return (
     <div className="container mx-auto flex flex-wrap justify-start">
@@ -24,33 +39,27 @@ const CommentList = ({ commentList, memoDataList, path }) => {
                 }
                 style={{ cursor: "pointer" }}>
                 <div className="flex justify-between">
-                  <h2 className="card-title">
-                    {receivedComment ? "[내가 받은 댓글]" : "[내가 쓴 댓글]"}
-                  </h2>
+                  <h2 className="card-title">{commentTitleText()}</h2>
                 </div>
                 <p>{elem.content}</p>
               </div>
-              {receivedComment ? (
-                <></>
-              ) : (
-                <>
-                  <div className="flex justify-end mt-10">
-                    <div
-                      onClick={() => navigate("/my/edit-comment", { state: { data: elem } })}
-                      className="btn btn-sm mx-1">
-                      댓글 수정
-                    </div>
-                    <div
-                      onClick={() =>
-                        navigate("/my/delete-comment", {
-                          state: { seq: elem.seq, storySeq: elem.story_seq },
-                        })
-                      }
-                      className="btn btn-sm btn-error mx-1">
-                      댓글 삭제
-                    </div>
+              {!receivedComment && viewId === userId && (
+                <div className="flex justify-end mt-10">
+                  <div
+                    onClick={() => navigate("/my/edit-comment", { state: { data: elem } })}
+                    className="btn btn-sm mx-1">
+                    댓글 수정
                   </div>
-                </>
+                  <div
+                    onClick={() =>
+                      navigate("/my/delete-comment", {
+                        state: { seq: elem.seq, storySeq: elem.story_seq },
+                      })
+                    }
+                    className="btn btn-sm btn-error mx-1">
+                    댓글 삭제
+                  </div>
+                </div>
               )}
             </div>
           </div>
